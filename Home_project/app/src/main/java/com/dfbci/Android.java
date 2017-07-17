512dd438364e7d5e67687fc20ad347df060be10a
parimaladesai <parimala993@gmail.com>
2017-07-17 16:55:20 +0200

    First commit to master

---
+++ Home_project/app/src/main/java/com/dfbci/Android.java
@@ -0,0 +1,7 @@
+package com.dfbci;
+
+/**
+ * Created by admin on 17-07-2017.
+ */
+public class Android {
+}
---
+++ Home_project/app/src/main/java/com/dfbci/Tab3Fragment.java
@@ -0,0 +1,105 @@
+package com.dfbci;
+
+
+
+import android.support.v4.app.Fragment;
+
+import android.os.Bundle;
+
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+
+public class Tab3Fragment extends Fragment {
+
+    // TODO: Rename and change types and number of parameters
+    public static Tab3Fragment newInstance() {
+        Tab3Fragment fragment = new Tab3Fragment();
+
+        return fragment;
+    }
+
+    private ListView listView;
+    private String names[] = {
+            "Video1",
+            "Video2",
+            "Video3",
+            "Video4",
+            "Video5",
+            "Video6",
+            "Video7",
+            "Video8"
+    };
+
+    private String desc[] = {
+            "EMPTINESS FT.JUSTIN TIBLEKAR",
+            "I'M FALLING LOVE WITH YOU",
+            "BABY FT.JUSTIN BABER",
+            "EMPTINESS FT.JUSTIN TIBLEKAR",
+            "I'M FALLING LOVE WITH YOU",
+            "BABY FT.JUSTIN BABER",
+            "EMPTINESS FT.JUSTIN TIBLEKAR",
+            "I'M FALLING LOVE WITH YOU",
+            "BABY FT.JUSTIN BABER",
+
+    };
+
+
+    private Integer imageid[] = {
+            R.drawable.pic11,
+            R.drawable.pic11,
+            R.drawable.pic11,
+            R.drawable.pic11,
+            R.drawable.pic11,
+            R.drawable.pic11,
+            R.drawable.pic11,
+            R.drawable.pic11
+    };
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View myFragmentView = inflater.inflate(R.layout.fragment_tab1, container, false);
+
+
+
+
+        CustomList customList = new CustomList(getActivity(), names, desc, imageid);
+
+        listView = (ListView) myFragmentView.findViewById(R.id.listView);
+        listView.setAdapter(customList);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getActivity(),"You Clicked "+names[i],Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return myFragmentView;
+    }
+
+
+
+   /* @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }*/
+}
+
